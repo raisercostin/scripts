cb57e5eab79c9d3bef7e939aed9d8249da7d5851
Costin Grigore <raisercostin@gmail.com>
2025-07-10 08:18:07 +0300

    add utf8 as default

--- mvn2gradle.java
+++ mvn2gradle.java
@@ -935,6 +935,7 @@ public class mvn2gradle {
 
           tasks.withType<JavaCompile> {
               options.compilerArgs.add("-Xlint:unchecked")
+              options.encoding = "UTF-8"
           }
 
           dependencies {
